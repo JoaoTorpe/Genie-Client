d2072a6777cd8b8112f994c1a7ef10970fc3acd8
JoaoTorpe <victotorpe10@gmail.com>
2024-06-01 20:33:48 -0300

    Option component

---
+++ src/Components/Option.tsx
@@ -0,0 +1,45 @@
+
+import "./OptionCss.css"
+
+interface OptionProps{
+    answer:string
+    letterNumber:number
+    correct:Boolean
+} 
+
+
+const Option: React.FC<OptionProps> = ({answer,letterNumber,correct})=>{
+let letter:any;
+
+switch (letterNumber) {
+    case 1:
+      letter = 'A';
+      break;
+    case 2:
+      letter = 'B';
+      break;
+    case 3:
+      letter = 'C';
+      break;
+    case 4:
+      letter = 'D';
+      break;
+    case 5:
+        letter = 'E'
+      break
+
+    default:
+      letter = 'N/A';
+      break;
+  }
+
+return(
+<div className="optionBody" data-isCorrect={correct} >
+<button  >{letter}</button>
+<h3>{answer}</h3>
+</div>
+);
+}
+
+
+export default Option
