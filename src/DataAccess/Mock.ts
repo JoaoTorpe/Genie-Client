4affa00c58e79dbc574f0ea21a2cadc3046e8b0d
JoaoTorpe <victotorpe10@gmail.com>
2024-06-02 19:01:34 -0300

    Mock data for testing

---
+++ src/DataAccess/Mock.ts
@@ -0,0 +1,61 @@
+
+export const data = [
+    {"materia":"matematica",
+     "assunto":"juros",
+     "dificuldade":"facil",
+        "enunciado": "Ana decidiu investir R$ 1.500,00 em um aplicativo de renda fixa que oferece juros simples de 1% ao mês. Quantos reais ela receberá após 6 meses de investimento?",
+        "alternativas": [
+          { "texto": "R$ 1.590,00", "correta": true },
+          { "texto": "R$ 1.650,00" },
+          { "texto": "R$ 1.710,00" },
+          { "texto": "R$ 1.800,00" }
+        ],
+        "dica": "Para resolver este problema, podemos utilizar a regra de três simples, que estabelece uma proporcionalidade entre duas grandezas.",
+        "resolucao": [
+          "**Etapa 1: Montar a proporção.**",
+          "Metros de muro | Tempo (horas)",
+          "------- | --------",
+          "8 | 6",
+          "12 | x",
+          "**Etapa 2: Multiplicar os termos na diagonal.**",
+          "8 * x = 6 * 12",
+          "**Etapa 3: Resolver para x.**",
+          "x = (6 * 12) / 8",
+          "x = 9",
+          "**Etapa 4: Responder à pergunta.**",
+          "O pedreiro levará **9 horas** para construir 12 metros de muro."
+        ]
+
+      },
+      {
+        "materia": "matematica",
+        "assunto": "regra de tres simples",
+        "dificuldade": "media",
+        "enunciado": "Um pedreiro precisa de 6 horas para construir 8 metros de muro. Quantas horas ele levará para construir 12 metros de muro?",
+        "alternativas": [
+          { "texto": "4 horas" },
+          { "texto": "5 horas" },
+          { "texto": "7 horas", "correta": true },
+          { "texto": "9 horas" },
+          { "texto": "10 horas" }
+        ],
+        "dica": "Para resolver este problema, podemos utilizar a regra de três simples, que estabelece uma proporcionalidade entre duas grandezas.",
+        "resolucao": [
+          "**Etapa 1: Montar a proporção.**",
+          "Metros de muro | Tempo (horas)",
+          "------- | --------",
+          "8 | 6",
+          "12 | x",
+          "**Etapa 2: Multiplicar os termos na diagonal.**",
+          "8 * x = 6 * 12",
+          "**Etapa 3: Resolver para x.**",
+          "x = (6 * 12) / 8",
+          "x = 9",
+          "**Etapa 4: Responder à pergunta.**",
+          "O pedreiro levará **9 horas** para construir 12 metros de muro."
+        ]
+      }
+      
+
+
+]
