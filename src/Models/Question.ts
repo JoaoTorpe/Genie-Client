672bcd5d9fb821a354ee8e8801016774e6410d17
JoaoTorpe <victotorpe10@gmail.com>
2024-06-15 16:22:28 -0300

    Refactor: Question entity refactor

--- src/Models/Question.ts
+++ src/Models/Question.ts
@@ -7,17 +7,15 @@ export class Question{
     statement:Statement
     options:Option[]
     tip:string
-    solve:string[]
     id:Number
     correct:boolean
 
-constructor(header:Header,statement:Statement,options:Option[],tip:string,solve:string[],id:number,correct:boolean){
+constructor(header:Header,statement:Statement,options:Option[],tip:string,id:number,correct:boolean){
 
 this.header = header
 this.statement = statement
 this.options=options
 this.tip = tip
-this.solve = solve
 this.id = id
 this.correct = correct
 }
